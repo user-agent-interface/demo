892d05c40284c8cfbec51ea6f459abbb9f76b787
daniel.szuk <daniel.szuk@futuristic.digital>
2026-02-04 14:43:35 +0100

    refactor: move uai-client to packages

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@uai/client": path.resolve(__dirname, "../uai-client/src/index.ts"),
+      '@': path.resolve(__dirname, './src'),
+      '@uai/client': path.resolve(
+        __dirname,
+        '../../packages/uai-client/dist/index.js'
+      ),
     },
   },
   server: {
-    port: 5173
+    port: 5173,
   },
 });
