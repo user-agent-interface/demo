59f3794728527726b48c78265e730d056b8e8bfd
daniel.szuk <daniel.szuk@futuristic.digital>
2026-02-25 15:12:04 +0100

    fix(uai-client): use correct  zodSchema function

--- packages/uai-client/src/use-uai/component-map-for-server.conversion.ts
+++ packages/uai-client/src/use-uai/component-map-for-server.conversion.ts
@@ -1,21 +1,27 @@
 import { ComponentMapForServer } from '@uai/shared';
 import { ComponentMap } from '../component-map/component-map';
-import { asSchema } from 'ai';
+import { zodSchema } from 'ai';
 
-export const convertComponentMapForServer = (componentMap: ComponentMap) => {
+export const convertComponentMapForServer = async (
+  componentMap: ComponentMap
+) => {
   const componentMapForServer: ComponentMapForServer = {};
+
   // convert the tool definition to an interpretable format for the UAI server (e.g. JSON schemas)
-  Object.entries(componentMap).forEach(async ([componentKey, component]) => {
+  for (const [componentKey, component] of Object.entries(componentMap)) {
+    const inputSchema = await zodSchema(component.inputSchema).jsonSchema;
+    const outputSchema = component.outputSchema
+      ? await zodSchema(component.outputSchema).jsonSchema
+      : undefined;
+
     componentMapForServer[componentKey] = {
       title: component.title,
       description: component.description,
       inputExamples: component.inputExamples,
-      inputSchema: await asSchema(component.inputSchema).jsonSchema,
-      outputSchema: component.outputSchema
-        ? await asSchema(component.outputSchema).jsonSchema
-        : undefined,
+      inputSchema,
+      outputSchema,
     };
-  });
+  }
 
   return componentMapForServer;
 };
